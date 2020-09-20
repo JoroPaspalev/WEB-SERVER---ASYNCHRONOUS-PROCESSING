b4bb620d2d7736b41130ec74c9a249329d008aa7
Joro <joro_paspalev@abv.bg>
2020-09-20 21:55:47 +0300

    Add .css file to Web site

---
+++ WEB SERVER - ASYNCHRONOUS PROCESSING/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WEB_SERVER___ASYNCHRONOUS_PROCESSING
+{
+    class Program
+    {
+        public static void BlockConsole()
+        {
+            // искам да намеря колко са простите числа от 1 до 100000 като бройка
+            int count = 0;
+            
+            List<int> primeNumbers = new List<int>();
+
+            for (int i = 2; i < 150000; i++)
+            {
+                bool isPrime = true;
+
+                for (int p = 2; p <= i - 1; p++)
+                {
+                    if (i % p == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    count++;
+                    primeNumbers.Add(i);
+                }                
+            }
+
+            Console.WriteLine(count);
+            Console.WriteLine("[" + string.Join(", ", primeNumbers) + "]");
+            
+        }
+
+        static void PrintInfo()
+        {
+            for (int i = 0; i <4; i++)
+            {
+                Console.WriteLine($"value of i: {i}");
+            }
+            Console.WriteLine("Child Thread Completed");
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(PrintInfo);
+            t1.Start();
+            Console.WriteLine("Main Thread Completed");
+
+
+            ;
+
+
+
+
+            // Искам да направя метод който като го изпълня да блокира Console.Readline за определено време - например 5 сек. Затова си правя BlockConsole
+            Console.Write("write your name:");
+            Console.ReadLine();
+
+           
+            Task task = Task.Run(() => 
+            {
+                BlockConsole(); 
+            });
+
+            Console.Write("write your lastname:");
+            Console.ReadLine();
+
+            Console.Write("your age:");
+            Console.ReadLine();
+
+            Console.Write("your town:");
+            Console.ReadLine();
+
+            ;
+
+
+
+
+
+
+
+            Task<long> task1 = Task<long>.Run(() =>
+            {
+                long sum = 0;
+                for (int i = 0; i < 20000; i++) sum += i;
+                return sum;
+            });
+
+            Console.WriteLine(task1.Result);
+
+
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+
+                    }
+                });
+            }
+
+            Console.WriteLine(sw.Elapsed);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01HTTPServerDemo
+{
+    class Program
+    {
+        const string NewLine = "\r\n";
+
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient client = await tcpListener.AcceptTcpClientAsync();
+
+                await ProcessClientAsync(client);
+            }
+
+
+        }
+
+        public static async Task ProcessClientAsync(TcpClient client)
+        {
+            using (var stream = client.GetStream())
+            {
+                byte[] buffer = new byte[1000000000];
+
+                var lenght = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                string requestString = Encoding.UTF8.GetString(buffer, 0, lenght);
+
+                Console.WriteLine(requestString);
+
+                string html = $"<h1>Hello from NikiServer {DateTime.Now}</h1>" +
+                    $"<form action=/tweet method=post><input name=username /><input name=password />" +
+                    $"<input type=submit /></form>";
+
+                string htmlTest = await File.ReadAllTextAsync(@"..\..\..\index.html");
+
+
+
+                string response = string.Empty;
+
+                if (requestString.Contains("style.css"))
+                {
+                    string cssFromFile = await File.ReadAllTextAsync(@"..\..\..\style.css");
+
+                     response = "HTTP/1.1 200 OK" + NewLine +
+                    "Server: KitaecaServer 2020" + NewLine +
+                    "Content-Type: text/css; charset=utf-8" + NewLine +                   
+                    "Content-Lenght: " + cssFromFile.Length + NewLine +
+                    NewLine +
+                    cssFromFile + NewLine
+                    ;
+                }
+                else
+                {
+                     response = "HTTP/1.1 200 OK" + NewLine +
+                    "Server: KitaecaServer 2020" + NewLine +  
+                    "Content-Type: text/html; charset=utf-8" + NewLine +
+                    // "Content-Disposition: attachment; filename=kitaeca.txt" + NewLine +
+                    //"Content-Lenght: " + html.Length + NewLine +
+                    NewLine +
+                    htmlTest + NewLine
+                    ;
+                }
+
+
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                await stream.WriteAsync(responseBytes);
+
+
+
+                Console.WriteLine(new string('=', 70));
+            }
+        }
+
+        public static async Task ReadData()
+        {
+            //string url = "https://softuni.bg/courses/csharp-web-basics";
+            HttpClient httpClient = new HttpClient();
+
+            //var response = await httpClient.GetAsync(url);
+            //Console.WriteLine(response.StatusCode);
+            //Console.WriteLine(string.Join(Environment.NewLine,
+            //response.Headers.Select(x => x.Key + ": " + x.Value.First())));
+
+            string htmlTest = await File.ReadAllTextAsync(@"..\..\..\index.html");
+            Console.WriteLine(htmlTest);
+        }
+    }
+}
